dcb455cb52aee635100ef6bf6f1aed5406fdee40
Marcin Charmułowicz <giero-mach@o2.pl>
2017-05-28 21:55:12 +0200

    Working import/export team to string

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 var teamHeroes = new Team('user', localStorage);
+var allHeroes = new Team('all', sessionStorage);
+
 teamHeroes.load();
 
-var allHeroes = new Team('all', sessionStorage);
 if (false === allHeroes.load()) {
     $.getJSON("data/heroes_all.json", function (json) {
         json.forEach(function (heroStat) {
@@ -10,11 +11,6 @@ if (false === allHeroes.load()) {
         allHeroes.save();
     });
 }
-
-Number.prototype.format = function (n, x) {
-    var re = '\\d(?=(\\d{' + (x || 3) + '})+' + (n > 0 ? '\\.' : '$') + ')';
-    return this.toFixed(Math.max(0, ~~n)).replace(new RegExp(re, 'g'), '$&,');
-};
 
 function getHeroesTableComponent(team) {
     return {
@@ -327,19 +323,48 @@ new Vue({
         teamHeroes: teamHeroes
     },
     mounted: function () {
-        $('.action-menu').on('click', 'a', function (e) {
+        $('.team-actions-menu').on('click', 'a', function (e) {
             e.preventDefault();
 
+            // undo default navigation click behavior
             var $this = $(this);
             $this.parent('li').removeClass('active');
             $this.parents('.dropdown-right').addClass('active');
 
             switch ($this.data('action')) {
                 case 'import':
-                    alert('Will be done soon ;)');
+                    var prompt = bootbox.prompt({
+                        title: "Paste previous exported string with your team configuration.",
+                        inputType: 'textarea',
+                        callback: function (result) {
+                            var importedString = LZString.decompressFromEncodedURIComponent(result);
+
+                            if (!teamHeroes.loadFromString(importedString)) {
+                                bootbox.alert({
+                                    message: 'Invalid input string :('
+                                });
+                            }
+                        }
+                    });
+
+                    prompt.init(function (_$) {
+                        var $textarea = $(_$.find('textarea')[0]);
+                        $textarea.css('height', '300px');
+                    });
                     break;
+
                 case 'export':
-                    alert('Will be done soon ;)');
+                    var alert = bootbox.alert({
+                        title: "Copy this string to a text file, to save your team configuration.",
+                        message: '<textarea style="margin: 0px; height: 300px; width: 100%;"></textarea>'
+                    });
+                    alert.init(function (_$) {
+                        var $textarea = $(_$.find('textarea')[0]);
+                        $textarea.val(LZString.compressToEncodedURIComponent(localStorage.getItem('heroes::user')));
+                        setTimeout(function () {
+                            $textarea.select();
+                        }, 1000);
+                    });
                     break;
             }
         })
